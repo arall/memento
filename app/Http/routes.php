52674b4feeeb0abd166f98356ce64bcf4fc8a120
vagrant <vagrant@homestead>
2015-07-22 19:16:44 +0000

    Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('memory',    'MemoryController');
+Route::resource('tag',        'TagController');
+Route::resource('person',    'PersonController');
